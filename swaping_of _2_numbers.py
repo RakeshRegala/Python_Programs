f101b2ab089a1b9e2144b537d3dce6c13ab7dfdc
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-07-15 16:44:06 +0530

    Update swaping_of _2_numbers.py

--- swaping_of _2_numbers.py
+++ swaping_of _2_numbers.py
@@ -1,5 +1,5 @@
-a,b=int(input()),int(input())        //giving 2 numbers
+a,b=int(input("First Number:")),int(input("2nd number:"))
 a=a+b
 b=a-b
 a=a-b
-print(a,b)
+print("First number:",a,"\n2nd number:",b)
