c65f40f495d92f422d0530186c06711741bb0eb8
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-07-18 18:21:58 +0530

    Update Biggest_of_3_programs.py

--- Biggest_of_3_programs.py
+++ Biggest_of_3_programs.py
@@ -1,6 +1,6 @@
-a=int(input())
-b=int(input())
-c=int(input())
+a=int(input("Enter 1st number"))
+b=int(input("Enter 2nd number"))
+c=int(input("Enter 3rd number"))
 if a>b and a>c:
     print("Biggest number is",a)
 elif b>a and b>c:
