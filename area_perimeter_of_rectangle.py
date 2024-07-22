ec5bf5c909064ef1d1177cb0f157ab24f806bc81
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-07-22 14:41:38 +0530

    AREA AND PERIMETER OF A RECTANGLE.py

---
+++ area_perimeter_of_rectangle.py
@@ -0,0 +1,9 @@
+#Area and Perimeter of a Rectangle
+
+
+length=int(input("Enter the LENGTH of a side: "))
+breadth=int(input("Enter the BREADTH of a side: "))
+area=length*breadth
+perimeter=2*(length+breadth)
+print("Area and perimeter of the rectangle is: ",area,"and",perimeter,"respectively")
+
