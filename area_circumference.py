3db9294793dc61deb4b6855765de8ec503571d01
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-07-22 14:56:34 +0530

    AREA and CIRCUMFERENCE of a circle.py

---
+++ area_circumference.py
@@ -0,0 +1,13 @@
+#AREA AND CIRCUMFERENCE OF A CIRCLE
+
+#area=pie*(r*r)
+#circumference=2*(pie)*r
+
+r=int(input("Enter RADIUS of the circle: "))
+pie=22/7
+area=pie*(r*r)
+
+circumference=2*pie*r
+print("The AREA of the circle is: ",area)
+print("The circumference of the circle is: ",circumference)
+
