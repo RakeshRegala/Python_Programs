fa0f9d4843f09a107979f33cc0549c80e393c48b
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-07-25 19:45:06 +0530

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -14,3 +14,16 @@ while ll<=ul:                            #control b/w limits
     if f == 2:
         print(ll)
     ll=ll+1
+
+/*ll=int(input("enter a ll "))
+ul=int(input("enter a ul "))
+c=int(0)
+
+for x in range (ll,ul):
+    c=0
+    for y in range (1,ul):
+        if(x%y==0):
+            c=c+1
+    if c==2:
+        print(x)
+   */
