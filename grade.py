8676c92a2c3042790e74028b8c57cd7927cb828d
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-07-28 19:33:24 +0530

    Grades_of_a_student.py

---
+++ grade.py
@@ -0,0 +1,13 @@
+marks=int(input("Enter Marks: "))
+if marks>=90:
+    print("The grade is:A")
+elif marks>=80:
+    print("The grade is:B")
+elif  marks>=70:
+    print("The grade is:C")
+elif marks>=60:
+    print("The grade is:D")
+elif marks>=50:
+    print("The grade is:E")
+else:
+    print("FAILED")
