2382973c45f123e01abdc4780e1804f3b1caaaea
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-07-29 15:24:17 +0530

    Create Length_of_a_string.py

---
+++ Program3/Length_of_a_string.py
@@ -0,0 +1,8 @@
+#Finding the length and each letter in a string 
+
+word=input("Enter a string: ")
+
+print("The letters in the string are: ")
+for i in word:
+	print(i)
+print("The length of the string is:",len(word))
