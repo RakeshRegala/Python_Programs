5900ecd8a8d5319bd147515ce16c8eef73d19cb6
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-07-29 15:13:28 +0530

    Create Printing_a_multiplication_table.py

---
+++ Program3/Printing_a_multiplication_table.py
@@ -0,0 +1,19 @@
+#Printing a multiplication table
+
+#using for loop
+
+
+"""
+i=1
+n=int(input("Enter a number: "))
+for i in range(1,11):
+	print(n,"X",i,"=",n*i)
+"""	
+	
+	
+	# using while loop
+n=int(input("Enter a number: "))
+i=1
+while i<=10:
+	print(f"{n} X {i} ={n*i}")
+	i=i+1
