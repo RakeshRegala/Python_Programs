7aeee4304ccebe5c7c90924159cdf12f9bce5a42
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-09-30 12:11:33 +0530

    Update Remove 1st occurance.py

--- Program5[Lists]/Remove 1st occurance.py
+++ Program5[Lists]/Remove 1st occurance.py
@@ -1,1 +1,16 @@
 
+list=[]
+
+n=int(input("Enter the number of elements: "))
+print("Enter",n,"elements")
+
+for i in range(n):
+    ele=int(input())
+    list.append(ele)
+    
+r=int(input("Enter element to be removed "))
+for i in list:
+    if r==i:
+        list.remove(i)
+        break
+print(list)
