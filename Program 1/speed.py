b4ed387a178b06c16a5cb7b81730d225182a769f
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-11-08 09:25:39 +0530

    Update speed.py

--- Program 1/speed.py
+++ Program 1/speed.py
@@ -1,4 +1,5 @@
-#FIND THE SPEED FOR GIVEN DISTANCE(km) AND TIME(min)
+ '''Write a program to find the speed when distance in kilometers and time in minutes
+are given.'''
 
 distance=int(input("Enter the DISTANCE in km: "))              #input in kilometers
 time=int(input("Enter the Time: "))         #input time in minutes
