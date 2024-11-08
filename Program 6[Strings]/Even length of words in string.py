c8845d8f99bca1e63f7e2d3f94baeb6b56393963
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-11-08 09:21:31 +0530

    Update Even length of words in string.py

--- Program 6[Strings]/Even length of words in string.py
+++ Program 6[Strings]/Even length of words in string.py
@@ -2,3 +2,13 @@ str=input("Enter a string: ")
 words=str.split()
 even_list=[test for test in words if len(test)%2 == 0]
 print("Even length words are:",even_list)
+
+
+
+'''str=input()
+words=str.split()
+even=[]
+for test in words:
+    if len(test) % 2 == 0:
+        even.append(test)
+print(even)'''
